bee5c23ffc5aa10ac4118fc378f2d07c6eea4e48
Şafak <58888062+akdemirsafak@users.noreply.github.com>
2022-03-01 01:33:26 +0300

    AverageCalculator_KdvHesaplama

--- KdvHesaplama/src/KdvCalculate.java
+++ KdvHesaplama/src/KdvCalculate.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class KdvCalculate {
+import java.util.Scanner;
+
+public class KdvCalculate {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("Kdv'siz Tutarı giriniz : ");
+        float price=input.nextFloat();
+
+        if (price>1000)
+        {
+            price*=1.08;
+
+        }
+        else
+        {
+
+           price*=1.18;
+        }
+
+        System.out.println(price);
+
+    }
 }
